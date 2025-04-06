b1f76f21356043afb45606495eabf98fe44e7974
Andy <andy_garlop4@hotmail.com>
2025-04-06 11:59:54 -0600

    Actualizacion de seccion de proyectos comming soon

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -17,18 +17,18 @@ export function Proyectos() {
 
                                 <div className='flex flex-col lg:flex-row md:gap-x-5 justify-center items-center py-4 border-b border-b-gray-400'>
 
-                                    <div>
+                                    <div className="flex flex-col justify-center items-center">
                                         <h4 className='font-bold text-[1.5rem] p-2 text-white/90'>{project.title}</h4>
-                                        <a href={project.url} target="_blank">
+                                        <a href={project.url} target="_blank" className="my-2">
                                             <img className='glass-object rounded-2xl an w-[20rem] md:w-[30rem] hover:animate-pulse' width="100px" height="100px" src={project.img} alt="imagen-proyecto-happyPet-web" />
                                         </a>
                                     </div>
 
                                     <div className="w-[20rem] md:w-[30rem] mt-5 text-center md:text-left">
                                         <p className='pt-2 text-white/90'>{project.description}</p>
-                                        <p className="my-3 text-white">{project.technologiesUse}:</p>
-                                        <p className='text-[#d6fd49]'>{project.technologie01}: <span className="text-white">{project.descrptionTechnologie01}</span></p>
-                                        <p className='text-[#38bdf8]'>{project.technologie02}: <span className="text-white"> {project.descrptionTechnologie02}.</span></p>
+                                        <p className="my-3 text-white">{project.technologiesUse}</p>
+                                        <p className='text-[#d6fd49]'>{project.technologie01} <span className="text-white">{project.descrptionTechnologie01}</span></p>
+                                        <p className='text-[#38bdf8]'>{project.technologie02} <span className="text-white"> {project.descrptionTechnologie02}</span></p>
 
                                         <p></p>
                                     </div>
--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -5,11 +5,11 @@ export const projects: Projects[] = [
     title: "Happy :) Pet",
     img: "img/happypet-web.webp",
     description:
-      "Desarrollo de una simulación de un sitio web para la gestión de citas de mascotas, que permite a los usuarios agendar, editar y eliminar citas de manera intuitiva. La plataforma ofrece una experiencia fácil de usar para mantener un seguimiento de las citas y actividades relacionadas con el cuidado de las mascotas",
+      "Desarrollo de una simulación de un sitio web para la gestión de citas de mascotas, que permite a los usuarios agendar, editar y eliminar citas de manera intuitiva. La plataforma ofrece una experiencia fácil de usar para mantener un seguimiento de las citas y actividades relacionadas con el cuidado de las mascotas.",
     technologiesUse: "Tecnologias Usadas",
     technologie01: "ViteJS:",
     descrptionTechnologie01:
-      "Para un desarrollo rápido y eficiente, aprovechando su rendimiento optimizado en la construcción y recarga de la aplicación",
+      "Para un desarrollo rápido y eficiente, aprovechando su rendimiento optimizado en la construcción y recarga de la aplicación.",
     technologie02: "TailwindCSS",
     descrptionTechnologie02:
       "Para un diseño responsivo y atractivo, asegurando que la interfaz sea visualmente agradable y funcional en todos los dispositivos.",
@@ -25,13 +25,29 @@ export const projects: Projects[] = [
     technologiesUse: "Tecnologias Usadas",
     technologie01: "React/TypeScript",
     descrptionTechnologie01:
-      "Para un desarrollo rápido y eficiente, aprovechando su rendimiento optimizado en la construcción y recarga de la aplicación",
+      "Para un desarrollo rápido y eficiente, aprovechando su rendimiento optimizado en la construcción y recarga de la aplicación.",
     technologie02: "TailwindCSS",
     descrptionTechnologie02:
       "Para un diseño responsivo y atractivo, asegurando que la interfaz sea visualmente agradable y funcional en todos los dispositivos.",
     id: 2, 
     url: "https://borcelle-salud-belleza.netlify.app/",
     alt: "imagen-proyecto-borcelle"
+  },
+  {
+    title: "Pagina sobre la buena alimetacion y contador de calorias",
+    img: "img/comming soon.webp",
+    description:
+      "",
+    technologiesUse: "",
+    technologie01: "",
+    descrptionTechnologie01:
+      "",
+    technologie02: "",
+    descrptionTechnologie02:
+      "",
+    id: 3, 
+    url: "",
+    alt: "imagen-proyecto-alimentacion"
   },
 
 ];
